76ec0e4446bf42d485a2f78af90462cf363f01d3
Alexandertp <sandertp@gmail.com>
2022-07-04 14:19:40 +0200

    Initial commit

---
+++ TicTacToe/Program.cs
@@ -0,0 +1,115 @@
+﻿// See https://aka.ms/new-console-template for more information
+//This applicaton will play the game Tic Tac Toe
+
+//Main board function
+string[] pos = { " "," "," "," "," "," "," "," "," " };
+//Denne bool styrer hvis tur det er, true = kryds, false = bolle
+bool turn = true;
+string brik = "X";
+int KrydsWins = 0;
+int BolleWins = 0;
+
+Console.Title = "Tic Tac Toe";
+
+while (true)
+{
+    PrintBoard();
+    if (turn)
+    {
+        brik = "X";
+    }
+    if (!turn)
+    {
+        brik = "O";
+    }
+    PlaceBrick(turn);
+    CheckVictory(turn);
+    turn = !turn;
+}
+
+void CheckVictory(bool Tur)
+{
+    if (((brik == pos[0] && brik == pos[1] && brik == pos[2]) || (brik == pos[0] && brik == pos[3] && brik == pos[6]) || (brik == pos[3] && brik == pos[4] && brik == pos[5])||(brik == pos[6] && brik == pos[7] && brik == pos[8])|| (brik == pos[1] && brik == pos[4] && brik == pos[7])||(brik == pos[2] && brik == pos[5] && brik == pos[8]) ||(brik == pos[2] && brik == pos[4] && brik == pos[6])||(brik == pos[0] && brik == pos[4] && brik == pos[8])))
+    {
+        if (Tur)
+        {
+            PrintBoard();
+            Console.WriteLine("X wins!\n");
+            KrydsWins++;
+        }
+        if (!Tur)
+        {
+            PrintBoard();
+            Console.WriteLine("O wins! \n");
+            BolleWins++;
+        }
+        Console.WriteLine("Vil du spille igen?");
+        string input = Console.ReadLine();
+        if (input.ToLower() == "yes" || input.ToLower() == "ja")
+        {
+            ResetBoard();
+        }
+        else
+        {
+            
+        }
+    }
+}
+void PrintBoard()
+{
+    Console.Clear();
+    Console.WriteLine($" {pos[0]} | {pos[1]} | {pos[2]} ");
+    Console.WriteLine($"----------");
+    Console.WriteLine($" {pos[3]} | {pos[4]} | {pos[5]} ");
+    Console.WriteLine($"----------");
+    Console.WriteLine($" {pos[6]} | {pos[7]} | {pos[8]} ");
+}
+
+void PlaceBrick(bool Tur)
+{
+    bool cont = true;
+    do
+    {
+
+        Console.WriteLine("Vælg et tal mellem 1 og 9");
+        int input = 0;
+        do
+        {
+            try
+            {
+                input = int.Parse(Console.ReadLine());
+                cont = true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Invalid input, prøv igen.");
+                cont = false;
+            }
+            
+        } while (cont == false);
+
+        if (input > 9 || input < 1)
+        {
+            Console.WriteLine("Invalid input, prøv igen.");
+            cont = false;
+        }
+        else if (pos[input - 1] == " ")
+        {
+            pos[input - 1] = brik;
+            cont = true;
+        }
+        else
+        {
+            PrintBoard();
+            Console.WriteLine("Den brik er allerede taget. Vælg en anden.");
+            cont = false;
+        }
+    } while (cont == false);
+}
+void ResetBoard()
+{
+    for (int i = 0; i < pos.Length; i++)
+    {
+        pos[i] = " ";
+    }
+}
